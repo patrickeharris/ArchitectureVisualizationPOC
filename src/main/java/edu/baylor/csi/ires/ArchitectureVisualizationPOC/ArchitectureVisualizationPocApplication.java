ece50cc3e44683465817fd3c1d7654cac34edcea
patrickeharris <patrickeharris404@gmail.com>
2022-06-16 10:02:57 +0200

    Initial commit

---
+++ src/main/java/edu/baylor/csi/ires/ArchitectureVisualizationPOC/ArchitectureVisualizationPocApplication.java
@@ -0,0 +1,13 @@
+package edu.baylor.csi.ires.ArchitectureVisualizationPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureVisualizationPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureVisualizationPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/baylor/csi/ires/ArchitectureVisualizationPOC/ArchitectureVisualizationPocApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.baylor.csi.ires.ArchitectureVisualizationPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureVisualizationPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
