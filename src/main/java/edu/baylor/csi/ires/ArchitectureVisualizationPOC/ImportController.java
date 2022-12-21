1c1aa6d64e82184f2cf0f29b2333bebf2f748461
Gabriel Goulis <ninjagabe2003@gmail.com>
2022-12-20 18:49:26 -0600

    Creation of my new branch g-goulis with updated changes to remove references to System2 and extra index2 && index3 functions

--- src/main/java/edu/baylor/csi/ires/ArchitectureVisualizationPOC/ImportController.java
+++ src/main/java/edu/baylor/csi/ires/ArchitectureVisualizationPOC/ImportController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.beans.JavaBean;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,80 +38,5 @@ public class ImportController {
             model.addAttribute("output", jsonString);
         }
         return "convert";
-    }
-
-    @RequestMapping("/convert2")
-    public String index2(
-            @RequestParam(value = "json", required = false) String json,
-            Model model
-    ) {
-        model.addAttribute("json", json);
-        System2 data = new Gson().fromJson(json, System2.class);
-        if(data != null){
-            java.lang.System.out.println("A");
-            List<Link> links = new ArrayList<>();
-            for(int i = 0; i < data.getNodes().size(); i++) {
-                for(int j = 0; j < data.getNodes().get(i).getDependants().size(); j++){
-                    Link l = new Link();
-                    l.setSource(data.getNodes().get(i).getNodeName());
-                    l.setTarget(data.getNodes().get(i).getDependants().get(j));
-                    links.add(l);
-                }
-                java.lang.System.out.println(data.getNodes().get(i).getNodeName());
-            }
-            data.setLinks(links);
-            Gson gson = new GsonBuilder()
-                    .excludeFieldsWithoutExposeAnnotation()
-                    .create();
-            String jsonString = gson.toJson(data);
-            java.lang.System.out.println(jsonString);
-            model.addAttribute("output", jsonString);
-        }
-        else{
-            java.lang.System.out.println("B");
-            java.lang.System.out.println("null");
-        }
-        return "convert2";
-    }
-
-    @RequestMapping("/convert3")
-    public String index3(
-            @RequestParam(value = "json", required = false) String json,
-            Model model
-    ) {
-        model.addAttribute("json", json);
-        System2 data = new Gson().fromJson(json, System2.class);
-        if(data != null){
-            java.lang.System.out.println("A");
-            List<Link> links = new ArrayList<>();
-            for(int i = 0; i < data.getNodes().size(); i++) {
-                for(int j = 0; j < data.getNodes().get(i).getDependants().size(); j++){
-                    Link l = new Link();
-                    l.setSource(data.getNodes().get(i).getNodeName());
-                    l.setTarget(data.getNodes().get(i).getDependants().get(j));
-                    links.add(l);
-                }
-
-                for(int j = 0; j < data.getNodes().get(i).getDependsOn().size(); j++){
-                    Link l = new Link();
-                    l.setTarget(data.getNodes().get(i).getNodeName());
-                    l.setSource(data.getNodes().get(i).getDependsOn().get(j));
-                    links.add(l);
-                }
-                java.lang.System.out.println(data.getNodes().get(i).getNodeName());
-            }
-            data.setLinks(links);
-            Gson gson = new GsonBuilder()
-                    .excludeFieldsWithoutExposeAnnotation()
-                    .create();
-            String jsonString = gson.toJson(data);
-            java.lang.System.out.println(jsonString);
-            model.addAttribute("output", jsonString);
-        }
-        else{
-            java.lang.System.out.println("B");
-            java.lang.System.out.println("null");
-        }
-        return "convert2";
     }
 }
